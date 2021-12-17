1459243df0f6c7c40d1fe6a1245e41caead53ad9
Michael Michelotti <michael.michelotti4@gmail.com>
2021-12-16 19:19:04 -0800

    implement get all articles and projects

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,3 +1,5 @@
+const APIFeatures = require('../utils/apiFeatures');
+
 exports.createOne = (Model) => async (req, res, next) => {
   const doc = await Model.create(req.body);
 
@@ -10,26 +12,22 @@ exports.createOne = (Model) => async (req, res, next) => {
 };
 
 exports.getAll = (Model) => async (req, res, next) => {
-  // let filter = {};
-  // if (req.params.productId) filter = { product: req.params.productId };
+  let filter = {};
 
-  // const features = new APIFeatures(Model.find(filter), req.query)
-  //   .filter()
-  //   .sort()
-  //   .limitFields()
-  //   .paginate();
+  const features = new APIFeatures(Model.find(filter), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
 
-  // const doc = await features.query;
+  const doc = await features.query;
 
-  // // SEND RESPONSE
-  // res.status(200).json({
-  //   status: 'success',
-  //   results: doc.length,
-  //   data: {
-  //     data: doc,
-  //   },
-  // });
+  // SEND RESPONSE
   res.status(200).json({
-    status: 'project created',
+    status: 'success',
+    results: doc.length,
+    data: {
+      data: doc,
+    },
   });
 };
